f99eb6f94ec50a31d44e23e373894f4d013fa1e4
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2015-08-15 16:36:53 -0400

    Added DecrementCount method and an initial position variable

--- Assets/Scripts/ToolBarItem.cs
+++ Assets/Scripts/ToolBarItem.cs
@@ -1,7 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ToolBarItem : MonoBehaviour {
 	[SerializeField]
-	GameObject prefab; 
+	GameObject prefab;
+	[SerializeField]
+	int count = 10;
+
+	public Vector2 initialPos { get; set;}
+
+	// Use this for initialization
+	void Start () {
+		Text countText = this.GetComponentInChildren<Text>();
+		countText.text = count.ToString();
+
+		initialPos = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void DecrementCount(int countChange){
+		Text countText = this.GetComponentInChildren<Text>();
+		this.count -= countChange;
+		countText.text = count.ToString();
+	}
+
+
 }
