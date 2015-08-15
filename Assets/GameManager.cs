84f463f0ad930eca825cc032f38ee3ae8b043990
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2015-08-15 09:39:25 -0400

    Set up unity basic managers

---
+++ Assets/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	[SerializeField]
+	private MapManager m_MapManagerPrefab;
+	[SerializeField]
+	private AnimalController m_AnimalPrefab;
+}
