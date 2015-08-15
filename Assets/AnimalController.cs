b2f36f22b50d28ae9013bcd554460888224b800d
lisa <lisa.soros@eecs.ucf.edu>
2015-08-15 11:57:15 -0400

    Added collision detection and movement logic to animal controller.  (Still needs some testing - haven't integrated an actual sprite yet.)

--- Assets/AnimalController.cs
+++ Assets/AnimalController.cs
@@ -3,64 +3,86 @@ using System.Collections;
 
 public class AnimalController : MonoBehaviour {
 
+	#region instance variables
 	public enum Direction {North, South, East, West};
-	const int stepSize = 5;
+	const float stepSize = 0.1f;
 
-	public Vector2 velocity;
+	// public variables
 	public Direction direction;
+	public Vector2 velocity;
 
-	private Random rng;
+	// private variables 
+	// (reusable temp variables; saving memory)
+	private Collider2D[] collisionResults;
+	private int randDir;
+	#endregion
 
+	#region methods
 	// Use this for initialization
 	void Start () {
-		// Set initial direction to north
 		direction = Direction.North;
-
-		// Set the animal's speed
-
-		// Initialize the random number generator
-		rng = new Random();
+		velocity = new Vector2 (0.0f, stepSize);
+		collisionResults = new Collider2D[50];
 	}
 
 	// Update is called once per frame
 	// TODO: add lure attraction (later)
+	// TODO: collision handling for different types of colliders (walls, escape pods, lures)
 	void Update () {
 
-		// Check for collisions (tile-based) 
-		// If there is a collision, probabilistically change the animal's direction (can't stay in same direction)
-		// (use while loop to check if it's a valid direction - invalid directions are the old direction or a new
-		// direction that also causes a collision problem)
+		// Check for collisions
+		// For now, all objects are treated as walls
+		while (Physics2D.OverlapPointNonAlloc (transform.position + (Vector3)velocity, collisionResults) != 0) {
 
-
-		// Regardless of whether or not there are any collisions, move the animal forward according to its speed
-		switch (direction) {
-			case Direction.North:
-				velocity.x = 0;
-				velocity.y = -stepSize;
+			// If there would be a collision, probabilistically change the animal's direction (can't stay in same direction)
+			randDir = Random.Range (0, 3);
+			while (randDir==(int)direction)
+				randDir = Random.Range (0, 3);
+			
+			switch (randDir) {
+			case 0:
+				direction = Direction.North;
 				break;
-
-			case Direction.South:
-				velocity.x = 0;
-				velocity.y = stepSize;
+				
+			case 1:
+				direction = Direction.South;
 				break;
-
-			case Direction.East:
-				velocity.x = stepSize;
-				velocity.y = 0;
+				
+			case 2:
+				direction = Direction.East;
 				break;
-
-			case Direction.West:
-				velocity.x = -stepSize;
-				velocity.y = 0;
+				
+			case 3:
+				direction = Direction.West;
 				break;
+			}
+		
+			// Set the velocity according to the new direction
+			switch (direction) {
+				case Direction.North:
+					velocity.x = 0;
+					velocity.y = -stepSize;
+					break;
+
+				case Direction.South:
+					velocity.x = 0;
+					velocity.y = stepSize;
+					break;
+
+				case Direction.East:
+					velocity.x = stepSize;
+					velocity.y = 0;
+					break;
+
+				case Direction.West:
+					velocity.x = -stepSize;
+					velocity.y = 0;
+					break;
+			}
 		}
 
-		// Update the animal's position
-	
+		// Regardless of whether or not the velocity has changed, update the position
+		transform.position += (Vector3)velocity;
 	}
-
-	void OnCollisionEnter2D(Collision2D coll) {
-	}
-
-
+	#endregion
 }
