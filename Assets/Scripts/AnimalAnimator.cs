f38704d166082b26053e137a8f35710a14ae90a6
ruffenman <jorge@jorgeccapote.com>
2015-08-16 09:46:36 -0400

    - animal anims are working
    Signed-off-by: ruffenman <jorge@jorgeccapote.com>

--- Assets/Scripts/AnimalAnimator.cs
+++ Assets/Scripts/AnimalAnimator.cs
@@ -11,17 +11,7 @@ public class AnimalAnimator : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		int layerIndex = m_animator.GetLayerIndex(name);
-		for(int i=0; i < NUM_ANIMAL_TYPES; ++i)
-		{
-			if(i != 0 && i + 1 == layerIndex)
-			{
-				m_animator.SetLayerWeight(layerIndex, 1);
-			}
-			else
-			{
-				m_animator.SetLayerWeight(layerIndex, 0);
-			}
-		}
+		m_animator.SetLayerWeight(layerIndex, 1);
 	}
 	
 	// Update is called once per frame
